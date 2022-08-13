0ee22ab1a3370e13dde75a9d7f4cce0f6485db85
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-13 17:30:48 -0500

    Prefix and Postfix in Java

---
+++ FirstApp/src/PrefixPostfix.java
@@ -0,0 +1,13 @@
+public class PrefixPostfix {
+    public static void main(String[] args){
+        int someValue = 5;
+        System.out.println("The original value of someValue: " + someValue);
+        System.out.println("++someValue: " + ++someValue);
+        System.out.println("someValue: " + someValue);
+
+        int someOtherValue = 5;
+        System.out.println("The original value of someOtherValue: " + someOtherValue);
+        System.out.println("someOtherValue++: " + someOtherValue++);
+        System.out.println("someOtherValue: " + someOtherValue);
+    }
+}
