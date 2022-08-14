07cc57d7d00536fbfb9e6a403779c28bb3beca76
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-14 10:50:28 -0500

    Command line Arguments in Java

---
+++ Methods/src/CommandLineArguments.java
@@ -0,0 +1,11 @@
+public class CommandLineArguments {
+    public static void main (String[] args){
+        if(args.length < 1){
+            System.out.println("No args provided");
+        } else {
+            for(String arg : args){
+                System.out.println(arg);
+            }
+        }
+    }
+}
