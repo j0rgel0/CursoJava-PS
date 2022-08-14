42de35854cd392c1494c0060b9ec5cef9e1e17ef
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-14 01:00:57 -0500

    CalcEngine, array features

--- CalcEngine/src/Main.java
+++ CalcEngine/src/Main.java
@@ -1,32 +1,42 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        /*
         double value1 = 100.0d;
         double value2 = 50.0d;
         double result = 0.0d;
         char opCode = 'a';
+        */
 
-        switch(opCode){
-            case 'a':
-                result = value1 + value2;
-                opCode = '+';
-                break;
-            case 's':
-                result = value1 - value2;
-                opCode = '-';
-                break;
-            case 'm':
-                result = value1 * value2;
-                opCode = '*';
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0d;
-                opCode = '/';
-                break;
-            default:
-                System.out.println("Invalid opCode: " + opCode);
-                result = 0.0d;
-                break;
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'/', '+', '-', '*'};
+        double[] results = new double[opCodes.length];
+
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case '+':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case '-':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case '*':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case '/':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Invalid opCode: " + opCodes[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
         }
-        System.out.println(value1+" "+ opCode +" "+value2+" = "+result);
+
+        int index = 0;
+        for(double currentResult : results) {
+            System.out.println(leftVals[index] + " " + opCodes[index] + " " + rightVals[index] + " = " + results[index]);
+            index++;
+        }
     }
 }
