e2914ed99961a496c5e4242c5218f3a1867a7dd3
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-14 01:25:12 -0500

    Adding return to Parameters.class

--- Methods/src/Parameters.java
+++ Methods/src/Parameters.java
@@ -1,16 +1,19 @@
 public class Parameters {
     public static void main (String[] args){
         showSum(7.5f,1.4f,3);
+        System.out.println("Back from showSum");
     }
 
     static void showSum(float x, float y, int count){
+        if(count<1){
+            return;
+        }
         float sum = x + y; // sum: 8.9
         for(int i = 0; i < count; i++){
             System.out.println(sum);
+            return;
         }
         /*
-            8.9
-            8.9
             8.9
          */
     }
